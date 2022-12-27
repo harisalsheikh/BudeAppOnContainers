52c63aed86e6121d54c7813aa5d34d85fe758c6b
harisalsheikh <harisalsheikh@hotmail.com>
2022-12-27 17:33:50 +0800

    Budget Application Created

---
+++ src/BudgetAppOnContainers/BudgetAppOnContainers/Pages/Budget.razor.cs
@@ -0,0 +1,17 @@
+﻿using BudgetAppOnContainers.Services;
+
+namespace BudgetAppOnContainers.Pages
+{
+    public partial class Budget
+    {
+        public BudgetGoals budgetGoals { get; set; } = new BudgetGoals();
+
+        private int savings = 0;
+
+        private void CalculateAmountSavings()
+        {
+            savings = budgetGoals.TotalSaving / budgetGoals.Month;
+        }
+
+    }
+}
---
+++ src/BudgetAppOnContainers/BudgetAppOnContainers/Services/BudgetGoals.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetAppOnContainers.Services
+{
+    public class BudgetGoals
+    {
+        public int Month { get; set; }
+        public int Day => DateTime.DaysInMonth(DateTime.Now.Year, Month);
+        public int TotalSaving { get; set; }
+    }
+}
